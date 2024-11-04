d717a9fb2c76edfa460d11aad4744032fe3f6e79
lys7942 <lys7942@nyu.edu>
2024-11-03 19:30:58 -0500

    read me + folder changes

---
+++ fortune/__main__.py
@@ -0,0 +1,15 @@
+import fortune.FortuneCookie as FortuneCookie
+
+def main():
+    print("Welcome to Scooby's Fortunes!")
+
+    option = input("Do you want to generate random fortunes or create your own? (r for random, c for create): ")
+
+    if option != "c" and option != "r":
+        print("Invalid options")
+    else: 
+        FortuneCookie.cookieScript(option)
+
+
+if __name__ == "__main__":
+    main()
